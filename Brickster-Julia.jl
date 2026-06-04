478a6ebf4e35f020bde09ea9bda96f45792a0492
Spenser <spenseranderson2@gmail.com>
2026-06-04 12:06:51 +0200

    Added  Function list catalog

--- Brickster-Julia.jl
+++ Brickster-Julia.jl
@@ -11,10 +11,12 @@ using Match
 export BricksterClient, list_computes, compute_connect
 
 # Creating a struct to pass objects
-struct BricksterClient
-    token :: String
-    host_name :: String
-    warehouse_map :: Dict
+Base.@kwdef mutable struct BricksterClient
+    token :: String         # required keyword
+    host_name :: String     # required keyword
+    header :: Vector{Pair{String, String}}         # required keyword
+    warehouse_map :: Dict   
+    compute :: Union{String, Nothing} = nothing # this will be updated
 end 
 
 
@@ -37,25 +39,30 @@ function BricksterClient(token, host_name)
     warehouses_parsed = JSON3.read(warehouses.body)
     warehouse_map = Dict(w[:id] => w[:name] for w in warehouses_parsed[:warehouses])
 
-    BricksterClient(token, host_name, warehouse_map)
+    BricksterClient(token = token 
+                    ,host_name = host_name
+                    ,header = header
+                    ,warehouse_map = warehouse_map
+                    ,compute = nothing
+                    )
 end 
 
 # Function to list all computes within a warehouse
-function list_computes(warehouse :: BricksterClient)
+function list_computes(workspace :: BricksterClient)
     # Printing compute names
-    @printf("These are the available computes in your warehouse: %s",values(warehouse.warehouse_map))
+    @printf("These are the available computes in your warehouse: %s",values(workspace.warehouse_map))
     
 end
 
-function compute_connect(warehouse :: BricksterClient, compute_name )
+function compute_connect(workspace :: BricksterClient, compute_name )
 
     # Attributes
     compute_match_key = nothing
 
 
     # Trying to match to compute of Interest
-    for (k,v) in warehouse_map
-        if occursin(r"compute_name"i, v)
+    for (k,v) in workspace.warehouse_map
+        if occursin(Regex(compute_name, "i"), v)
             compute_match_key = k
             break   # Break the loop if there's a match
         end
@@ -63,13 +70,67 @@ function compute_connect(warehouse :: BricksterClient, compute_name )
 
     # Catching if Fails
     if isnothing(compute_match_key)
-        erorr("No Matching Databricks Compute for: $compute")
+        error("No Matching Databricks Compute for: $compute_name")
 
     else 
+        workspace.compute = compute_match_key
         @printf("This is the compute you selected %s",compute_match_key)
     end
 
-    return compute_match_key
+    # return updated struct
+    return workspace
+
+end
+
+# List the catalogs within a workspace
+function list_catalogs(workspace)
+
+    # Sending the request
+    catalog_response = HTTP.get(
+        "$host_name/api/2.1/unity-catalog/catalogs"
+        ,header
+        ,query = ["max_results" => "0"]
+    )
+
+    # Parsing the body
+    catalogs_parsed = JSON3.read(catalog_response.body)
+    catalog_tuple = [(
+                        name            = c[:name]
+                        ,full_name      = c[:full_name]
+                        ,id             = c[:id]
+                        ,owner          = c[:owner]
+                        ,created_date   = c[:created_at]
+                        ,created_by     = c[:created_by]
+                        ) 
+                    for c in catalogs_parsed[:catalogs]
+                    ]
+    # Creating a Dataframe                
+    cat_df = DataFrame(catalog_tuple)
+
+    # Add option to add DataFrame to struct rather than recreate it
+
+    # Printing the catalog names
+    print("These are the catalogs in your workspace")
+    for c in catalogs_parsed[:catalogs]
+        @printf("%s \n", c[:name])
+    end
+
+end
+
+# List the Schemas within a catalog
+
+
+function query_db(workspace :: BricksterClient, query :: String)
+
+# Sending the request to Databricks
+response = HTTP.post(
+    "$host/api/2.0/sql/statements"
+    ,header
+
+)
+
+
+# Creating a DataFrame
 
 end
 
