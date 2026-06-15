df3cf0b8f6a6c6077534188e2f10ca0882d3d616
Spenser <spenseranderson2@gmail.com>
2026-06-14 22:29:37 -0400

    Nearing 0.1 release. Queries can be created  - need to test

--- Brickster-Julia.jl
+++ Brickster-Julia.jl
@@ -8,15 +8,15 @@ using DataFrames
 using Match
 
 # Functions to be used
-export BricksterClient, list_computes, compute_connect, list_schemas, list_schemas
+export BricksterClient, list_computes, compute_connect, list_schemas, list_schemas, list_tables, query_db
 
 # Creating a struct to pass objects
 Base.@kwdef mutable struct BricksterClient
-    token :: String         # required keyword
-    host_name :: String     # required keyword
-    header :: Vector{Pair{String, String}}         # required keyword
+    token :: String         # Required keyword
+    host_name :: String     # Required keyword
+    header :: Vector{Pair{String, String}}         # Required keyword
     warehouse_map :: Dict   
-    compute :: Union{String, Nothing} = nothing # this will be updated
+    compute :: Union{String, Nothing} = nothing 
 end 
 
 
@@ -50,7 +50,7 @@ end
 # Function to list all computes within a warehouse
 function list_computes(workspace :: BricksterClient)
     # Printing compute names
-    @printf("These are the available computes in your warehouse: %s",values(workspace.warehouse_map))
+    @printf("These are the available computes in your warehouse: %s", values(workspace.warehouse_map))
     
 end
 
@@ -74,7 +74,7 @@ function compute_connect(workspace :: BricksterClient, compute_name )
 
     else 
         workspace.compute = compute_match_key
-        @printf("This is the compute you selected %s",compute_match_key)
+        @printf("This is the compute you selected %s", compute_match_key)
     end
 
     # return updated struct
@@ -83,85 +83,162 @@ function compute_connect(workspace :: BricksterClient, compute_name )
 end
 
 # List the catalogs within a workspace
-function list_catalogs(workspace)
+function list_catalogs(workspace :: BricksterClient, return_tuple :: Bool = false)
 
     # Sending the request
     catalog_response = HTTP.get(
-        "$host_name/api/2.1/unity-catalog/catalogs"
-        ,header
+        "$(workspace.host_name)/api/2.1/unity-catalog/catalogs"
+        ,workspace.header
         ,query = ["max_results" => "0"]
     )
 
     # Parsing the body
     catalogs_parsed = JSON3.read(catalog_response.body)
-    catalog_tuple = [(
-                        name            = c[:name]
-                        ,full_name      = c[:full_name]
-                        ,id             = c[:id]
-                        ,owner          = c[:owner]
-                        ,created_date   = c[:created_at]
-                        ,created_by     = c[:created_by]
-                        ) 
-                    for c in catalogs_parsed[:catalogs]
-                    ]
-    # Creating a Dataframe                
-    cat_df = DataFrame(catalog_tuple)
 
-    # Add option to add DataFrame to struct rather than recreate it
+    if return_tuple
 
-    # Printing the catalog names
-    print("These are the catalogs in your workspace:")
-    for c in catalogs_parsed[:catalogs]
-        @printf("%s \n", c[:name])
-    end
+        return [(
+                            name            = c[:name]
+                            ,full_name      = c[:full_name]
+                            ,id             = c[:id]
+                            ,owner          = c[:owner]
+                            ,created_date   = c[:created_at]
+                            ,created_by     = c[:created_by]
+                            ) 
+                        for c in catalogs_parsed[:catalogs]
+                        ]
+    else
+        # Printing the catalog names
+        print("These are the catalogs in your workspace:")
+
+        for c in catalogs_parsed[:catalogs]
+            @printf("%s \n", c[:name])
+        end
+    end 
 
 end
 
 # List the Schemas within a catalog
-function list_schemas(workspace)
+function list_schemas(workspace :: BricksterClient, catalog :: String, return_tuple :: Bool = false)
 
-    # sending he request
+    # sending the request
     schema_response = HTTP.get(
-        "$host_name/api/2.1/unity-catalog/schemas"
-        ,header
-        ,query = ["max_results" => "0"]
+        "$(workspace.host_name)/api/2.1/unity-catalog/schemas"
+        ,workspace.header
+        ,query = [
+                    "catalog_name" => "$catalog"
+                    ,"max_results" => "0"
+                ]
     )
 
-    # Parsing he body
+    # Parsing the body
     schemas_parsed = JSON3.read(schema_response.body)
-    catalog_tuple = [(
-                        name            = c[:name]
-                        ,full_name      = c[:full_name]
-                        ,id             = c[:id]
-                        ,owner          = c[:owner]
-                        ,created_date   = c[:created_at]
-                        ,created_by     = c[:created_by]
-                        ) 
-                    for c in catalogs_parsed[:catalogs]
-                    ]
 
+    # Creating tuple if arg
+    if return_tuple
+        return  [(
+                            name            = sch[:name]
+                            ,full_name      = sch[:full_name]
+                            ,id             = sch[:schema_id]
+                            ,owner          = sch[:owner]
+                            ,created_date   = sch[:created_at]
+                            ,created_by     = sch[:created_by]
+                            ) 
+                        for sch in schemas_parsed[:schemas]
+                        ]
+        
+    else 
+        println("These are your schemas:")
+
+        for sch in schemas_parsed[:schemas]
+            @printf("%s \n", sch[:name])
+        end
+    end
+
+
+end
+
+# List the tables within a schema
+function list_tables(workspace :: BricksterClient
+                        ,catalog :: String
+                        ,schema :: String
+                        ,omit_properties :: Bool = true
+                        ,return_tuple :: Bool = true )
+
+    # sending he request
+    tables_response = HTTP.get(
+        "$host_name/api/2.1/unity-catalog/tables"
+        ,workspace.header
+        ,query = [
+                    "catalog_name" => "$catalog"
+                    ,"schema_name" => "$schema"
+                    ,"omit_properties" => "$omit_properties" 
+                    ,"max_results" => "0"
+                ]
+    )
+
+    # Parsing the body
+    tables_parsed = JSON3.read(tables_response.body)
+
+    # Printing them 
+    println("These are the tables in the $catalog catalog...")
+    for tbl in tables_parsed[:tables]
+        @printf("%s \n", tbl[:full_name])
+    end
+
+    if return_tuple
+        # Creating the tuple
+        return [(
+                            name            = tbl[:name]
+                            ,full_name      = tbl[:full_name]
+                            ,id             = tbl[:table_id]
+                            ,owner          = tbl[:owner]
+                            ,created_date   = tbl[:created_at]
+                            ,created_by     = tbl[:created_by]
+                ) 
+                for tbl in tables_parsed[:tables]
+                ]
+        
+        end
 end
 
 function query_db(workspace :: BricksterClient, query :: String, schema :: String, catalog :: String)
 
     # Params 
     body = JSON3.write(Dict(
-        "statement"    => sql,
-        "warehouse_id" => client.compute,       # from your compute_connect step
-        "catalog"      => catalog,
-        "schema"       => schema,
-        "wait_timeout" => "30s",                # wait up to 30s for results
-        "disposition"  => "INLINE"              # return results directly in response
+        "statement"    => query
+        ,"warehouse_id" => client.compute       
+        ,"catalog"      => catalog
+        ,"schema"       => schema
+        ,"wait_timeout" => "30s"               
+        ,"disposition"  => "INLINE"             
     ))
-# Sending the request to Databricks
-response = HTTP.post(
-    "$host/api/2.0/sql/statements"
-    ,header
 
-)
+    # Sending the request to Databricks
+    response = HTTP.post(
+        "$(workspace.host_name)/api/2.0/sql/statements"
+        ,workspace.header
+        ,body
+
+    )
+
+    # Parsing the results
+    parsed_response = JSON3.read(response.body)
 
 
-# Creating a DataFrame
+    ## Creating a DataFrame
+    # Getting columns
+    df_cols = [col[:name] for col in parsed_response[:manifest][:schema][:columns]]
+
+    # Getting the rows
+    df_rows = [
+        [field[:str] for field in row] for row in parsed_response[:result][:data_array]
+                ]
+    
+    # Creating the df
+    df = DataFrame([col => [row[i] for row in df_rows] for (i, col) in enumerate(df_cols)])
+
+    return df 
 
 end
 
